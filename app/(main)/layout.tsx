d2b2549a3641cc264faab5d3f1ed93bb58c193f6
manhnth <themanh2k@gmail.com>
2024-06-24 13:57:47 +0700

    update:remove some trash code

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next';
 import AppLayoutClient from './layout.client';
 import { getServerUser } from '@/lib/getServerUser';
 import { redirect } from 'next/navigation';
+import Header from '@/components/header';
+import { Modal } from '@/components/modal';
+import { Sidebar } from '@/components/sidebar';
+import { getBudgetCategory } from '@/lib/queries';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,9 +20,16 @@ export default async function AppLayout({
   const user = await getServerUser();
 
   if (!user) redirect('/signin');
+
+  const data = await getBudgetCategory();
   return (
     <>
-      <AppLayoutClient>{children}</AppLayoutClient>
+      <Modal budgets={data?.budget} categories={data?.category} />
+      <Header />
+      <div className="md:flex md:gap-4">
+        <Sidebar />
+        <div className="h-[calc(100vh-48px)] md:grow">{children}</div>
+      </div>
     </>
   );
 }
